d07d132a13dc9c20580ae3678a42f8efa0f11b7f
Giora Guttsait <giora111@gmail.com>
2020-04-28 21:30:36 +0300

    Return a new auction on request (not persisted yet)

--- auction-service/src/handlers/createAuction.js
+++ auction-service/src/handlers/createAuction.js
@@ -1,7 +1,15 @@
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: new Date().toISOString(),
+  };
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 }
 
