cfaa8f16fbead5afee2d7d183e6e4087f579bfb3
Giora Guttsait <giora111@gmail.com>
2020-04-29 23:26:12 +0300

    add auth-service

---
+++ auth-service/src/handlers/auth.js
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+
+const generatePolicy = (principalId, Resource) => {
+  return {
+    principalId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: 'Allow',
+          Resource,
+        },
+      ],
+    },
+  };
+};
+
+export async function handler(event) {
+  if (!event.authorizationToken) {
+    throw 'Unauthorized';
+  }
+
+  const token = event.authorizationToken.replace('Bearer ', '');
+
+  try {
+    const claims = jwt.verify(token, process.env.AUTH0_PUBLIC_KEY);
+    const policy = generatePolicy(claims.sub, event.methodArn);
+
+    return {
+      ...policy,
+      context: claims
+    };
+  } catch (error) {
+    console.log(error);
+    throw 'Unauthorized';
+  }
+};
