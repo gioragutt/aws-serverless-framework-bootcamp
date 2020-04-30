57c21218580459c5deff8b8c4edf92e86bca6de3
Giora Guttsait <giora111@gmail.com>
2020-04-30 15:04:01 +0300

    send mail(s) when closing auctions

--- auction-service/src/lib/closeAuction.js
+++ auction-service/src/lib/closeAuction.js
@@ -1,6 +1,18 @@
+import AWS from 'aws-sdk';
 import { dynamodb, TableName } from './db';
 
+const sqs = new AWS.SQS();
+
 export async function closeAuction(auction) {
+  await updateAuctionInDb(auction);
+  if (auction.highestBid.bidder) {
+    await sendMailWhenAuctionHasBid(auction);
+  } else {
+    await sendMailWhenAuctionHasNoBid(auction);
+  }
+}
+
+async function updateAuctionInDb(auction) {
   await dynamodb.update({
     TableName,
     Key: { id: auction.id },
@@ -11,5 +23,46 @@ export async function closeAuction(auction) {
     ExpressionAttributeNames: {
       '#status': 'status',
     },
+  }).promise();
+}
+
+async function sendMailWhenAuctionHasBid({ title, seller, highestBid: { bidder, amount } }) {
+  const notifyBidder = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      recipient: bidder,
+      subject: `Congratulations on winning the bid on "${title}"! 🥳`,
+      body: `
+      Congratulations! 🎉
+      You're the highest bidder for "${title}"!
+      Your bid was $${amount}!
+      `
+    }),
+  }).promise();
+
+  const notifySeller = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      recipient: seller,
+      subject: `Your item "${title}" has been sold! 🥳`,
+      body: `
+      Congratulations! 🎉
+      Your item "${title}" has been sold!
+      It was sold for $${amount}! 🤑
+      `
+    }),
+  }).promise();
+
+  await Promise.all([notifyBidder, notifySeller]);
+}
+
+async function sendMailWhenAuctionHasNoBid({ title, seller, highestBid: { bidder, amount } }) {
+  await sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      recipient: seller,
+      subject: `Your item ${title} had no bidders 🙁`,
+      body: `Better luck next time!`
+    }),
   }).promise();
 }
